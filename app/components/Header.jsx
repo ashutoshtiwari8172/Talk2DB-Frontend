207b03a279e81160c3a25d608a0d280444a02156
Ashutosh <ashutoshtiwari8172@gmail.com>
2024-06-19 14:15:18 +0530

    chore: Update About page with justified text alignment

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -7,8 +7,9 @@ const Header = () => {
   return (
     <header className="bg-blue-900 text-white py-4 pt-16 ">
       <div className="container mx-auto flex">
-        <div className='my-10'>
-        <h1 className="text-4xl font-bold">Welcome to tok2dbs (Talk to Databases of Biomedical Science)</h1>
+        <div className='my-10 mx-10'>
+        <h1 className="text-4xl font-bold my-5">Welcome to tok2dbs </h1>
+        <h1 className='text-4xl font-bold mb-5'>Talk to Databases of Biomedical Science</h1>
         <p className="mt-2 text-2xl">Revolutionizing Data Access for Biomedical and Clinical Researchers</p>
         </div>
         <div>
