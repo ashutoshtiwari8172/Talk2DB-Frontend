5de035e58eef52790c698fd85b98f31af917bf3f
Ashutosh <ashutoshtiwari8172@gmail.com>
2024-06-19 14:24:42 +0530

    Update Header component with improved styling and layout

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -5,10 +5,10 @@ import AI from '../../public/images/ai.webp';
 
 const Header = () => {
   return (
-    <header className="bg-blue-900 text-white py-4 pt-16 ">
-      <div className="container mx-auto flex">
+    <header className="bg-blue-900 text-white py-4 pt-20 ">
+      <div className="container mx-auto flex my-5">
         <div className='my-10 mx-10'>
-        <h1 className="text-4xl font-bold my-5">Welcome to tok2dbs </h1>
+        <h1 className="text-4xl font-bold mt-10">Welcome to tok2dbs </h1>
         <h1 className='text-4xl font-bold mb-5'>Talk to Databases of Biomedical Science</h1>
         <p className="mt-2 text-2xl">Revolutionizing Data Access for Biomedical and Clinical Researchers</p>
         </div>
